b47af2370e1d709e4bb6f579f87c765f06f623f2
radykov <radykov>
2025-04-02 11:10:40 +0800

    Removed unused variable

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 // /src/components/Game.js
 import React from 'react';
-import { mobileView, MAX_WIDTH_STYLE } from '../styles';
+import { mobileView } from '../styles';
 import WordGame from './WordGame'
 import { GameProvider } from '../contexts/GameContext';
 
