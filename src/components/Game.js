056d6bdfafc09adb838bdeda3df36aebe6f8731f
radykov <radykov>
2025-04-02 14:24:06 +0800

    Removed outch blocking code

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,10 @@
 // /src/components/Game.js
-import React, { useEffect } from 'react';
+import React from 'react';
 import { mobileView } from '../styles';
 import WordGame from './WordGame'
 import { GameProvider } from '../contexts/GameContext';
 
 const Game = () => {
-    useEffect(() => {
-        // Add passive event listeners to improve touch handling
-        document.addEventListener('touchmove', (e) => { }, { passive: false });
-        document.addEventListener('touchstart', (e) => { }, { passive: true });
-
-        // Clean up
-        return () => {
-            document.removeEventListener('touchmove', (e) => { });
-            document.removeEventListener('touchstart', (e) => { });
-        };
-    }, []);
 
     return (
         <GameProvider>
